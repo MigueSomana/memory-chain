a7105811420593bacbdcecb7cdc1a5f3ae5462bd
MigueSomana <m.somana24@gmail.com>
2026-03-15 11:09:43 -0400

    ajuste leve al notfound

--- frontend/src/pages/simple/NotFound.jsx
+++ frontend/src/pages/simple/NotFound.jsx
@@ -1,10 +1,8 @@
 import React from "react";
-import NotFoundImg from "../../assets/notfound.png"
 
 const NotFound = () => {
   return (
     <div className="text-white">
-      <img src={NotFoundImg} width={700} />
       <h1>En proceso NotFound</h1>
     </div>
   );
